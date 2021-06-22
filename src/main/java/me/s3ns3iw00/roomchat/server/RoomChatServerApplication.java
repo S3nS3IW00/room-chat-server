25b536cc60251d6fe5d2eead77b726a30820daf0
S3nS3IW00 <danielgellert5@gmail.com>
2021-06-22 14:19:55 +0200

    Initial spring project

---
+++ src/main/java/me/s3ns3iw00/roomchat/server/RoomChatServerApplication.java
@@ -0,0 +1,13 @@
+package me.s3ns3iw00.roomchat.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomChatServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/s3ns3iw00/roomchat/server/RoomChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.s3ns3iw00.roomchat.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
